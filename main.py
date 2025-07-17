940f44d5abcea7b8e0648d99e69db025b5bfe665
guilhermesegura <guilhermesegura26@gmail.com>
2025-07-17 00:27:49 -0300

    improve the help message

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def statsDate(email: str, date: str):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-add", "--folder", help="add a new folder to scan for Git repositories")
-    parser.add_argument("-email", help="the email to scan")
+    parser.add_argument("-email", help="the email used to verify the commits owner")
     parser.add_argument("-date", "--startingdate", help="dd/mm/yyyy")
     parser.add_argument("-ls", "--list", help="List the folders that is being checked", action="store_true")
     parser.add_argument("-rm", "--remove", help="Remove the folder")
